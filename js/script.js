70fdb38bf25b55488f0ee24b362ec1b306b774c9
MonicaDeBona <debona.monica@outlook.it>
2022-12-05 16:07:40 +0100

    Add array and for loop

--- js/script.js
+++ js/script.js
@@ -19,8 +19,50 @@ Buon lavoro!
 Wayne Barnett  Founder & CEO     wayne-barnett-founder-ceo.jpg
 Angela Caroll  Chief Editor      angela-caroll-chief-editor.jpg
 Walter Gordon  Office Manager    walter-gordon-office-manager.jpg
-Angela Lopez   Social Media      Manager  angela-lopez-social-media-manager.jpg
+Angela Lopez   Social Media Manager  angela-lopez-social-media-manager.jpg
 Scott Estrada  Developer         scott-estrada-developer.jpg
 Barbara Ramos  Graphic Designer  barbara-ramos-graphic-designer.jpg
 */
 
+
+//MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
+const teamMember = [
+    {
+        nome : 'Wayne Barnett',
+        ruolo : 'Founder & CEO',
+        foto : 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome : 'Angela Caroll',
+        ruolo : 'Chief Editor',
+        foto : 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome : 'Walter Gordon',
+        ruolo : 'Office Manager ',
+        foto : 'walter-gordon-office-manager.jpg',
+    },
+    {
+        nome : 'Angela Lopez',
+        ruolo : 'Social Media Manager ',
+        foto : 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome : 'Scott Estrada',
+        ruolo : 'Developer',
+        foto : 'scott-estrada-developer.jpg',
+    },
+    {
+        nome : 'Barbara Ramos',
+        ruolo : 'Graphic Designer',
+        foto : 'barbara-ramos-graphic-designer.jpg',
+    },
+];
+
+//MILESTONE 1:
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+for (let i = 0; i < teamMember.length; i++) {
+    console.log(teamMember[i]);
+}
